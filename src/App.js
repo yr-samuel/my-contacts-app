4ba2e11dc8b2fdb12fbbc40d7ce36437f578d44d
yr-samuel <samuel.oliveira@eduzz.com>
2022-04-08 19:40:02 -0300

    initial styles config

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider } from 'styled-components';
+import defaultTheme from './assets/styles/themes/default';
+import GlobalStyles from './assets/styles/global';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<ThemeProvider theme={defaultTheme}>
+			<GlobalStyles />
+			<h1>Ola</h1>
+		</ThemeProvider>
+	);
 }
 
 export default App;
