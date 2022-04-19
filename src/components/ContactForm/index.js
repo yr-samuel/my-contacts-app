0dab5e80d21a8e203247854f728e2d575d21ea52
yr-samuel <samuel.oliveira@eduzz.com>
2022-04-19 19:30:19 -0300

    handle with form submit | change window title

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import FormGroup from '../FormGroup';
 import Input from '../Input';
 import Select from '../Select';
@@ -5,20 +6,35 @@ import Button from '../Button';
 import { ButtonContainer, Form } from './styles';
 
 export default function ContactForm({ buttonLabel }) {
+	const [name, setName] = useState('');
+	const [email, setEmail] = useState('');
+	const [phone, setPhone] = useState('');
+	const [category, setCategory] = useState('');
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		console.log({
+			name, email, phone, category,
+		});
+	};
+
 	return (
-		<Form>
+		<Form onSubmit={handleSubmit}>
 			<FormGroup>
-				<Input placeholder="Nome" />
-			</FormGroup>
-			<FormGroup error="O formato do email é inválido.">
-				<Input placeholder="Email" error />
+				<Input placeholder="Nome" value={name} onChange={(event) => setName(event.target.value)} />
 			</FormGroup>
 			<FormGroup>
-				<Input placeholder="Telefone" />
+				<Input placeholder="Email" value={email} onChange={(event) => setEmail(event.target.value)} />
 			</FormGroup>
 			<FormGroup>
-				<Select>
+				<Input placeholder="Telefone" value={phone} onChange={(event) => setPhone(event.target.value)} />
+			</FormGroup>
+			<FormGroup>
+				<Select value={category} onChange={(event) => setCategory(event.target.value)}>
+					<option value="">Categoria</option>
 					<option value="instagram">Instagram</option>
+					<option value="twitter">Twitter</option>
+					<option value="linkedin">Linkedin</option>
 				</Select>
 			</FormGroup>
 			<ButtonContainer>
