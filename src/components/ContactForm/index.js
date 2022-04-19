091d9791c5fb413b893d3fae63e76fa3adf59690
yr-samuel <samuel.oliveira@eduzz.com>
2022-04-19 18:58:02 -0300

    FormGroup added error message to after element chuldren | styled the input

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -10,8 +10,8 @@ export default function ContactForm({ buttonLabel }) {
 			<FormGroup>
 				<Input placeholder="Nome" />
 			</FormGroup>
-			<FormGroup>
-				<Input placeholder="Email" />
+			<FormGroup error="O formato do email é inválido.">
+				<Input placeholder="Email" error />
 			</FormGroup>
 			<FormGroup>
 				<Input placeholder="Telefone" />
--- src/components/FormGroup/index.js
+++ src/components/FormGroup/index.js
@@ -1,9 +1,10 @@
 import { Container } from './styles';
 
-export default function FormGroup({ children }) {
+export default function FormGroup({ children, error = null }) {
 	return (
 		<Container>
 			{children}
+			{error && <small>{error}</small>}
 		</Container>
 	);
 }
